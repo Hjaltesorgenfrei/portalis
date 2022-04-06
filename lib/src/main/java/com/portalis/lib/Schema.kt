636fc8938c00108dc00efb840b9a678940aa393c
Hjalte Sorgenfrei Mac Dalland <hjaltesorgenfrei@gmail.com>
2022-04-06 16:12:21 +0200

    Working on my own schema

--- lib/src/main/java/com/portalis/lib/Schema.kt
+++ lib/src/main/java/com/portalis/lib/Schema.kt
@@ -5,8 +5,9 @@ import com.github.victools.jsonschema.generator.OptionPreset
 import com.github.victools.jsonschema.generator.SchemaGenerator
 import com.github.victools.jsonschema.generator.SchemaGeneratorConfigBuilder
 import com.github.victools.jsonschema.generator.SchemaVersion
-import org.jetbrains.annotations.Nullable
 import java.io.File
+import kotlin.reflect.KClass
+import kotlin.reflect.KParameter
 
 
 object Schema {
@@ -21,5 +22,32 @@ object Schema {
         val jsonSchema: JsonNode = generator.generateSchema(Source::class.java)
 
         file.writeText(jsonSchema.toPrettyString())
+    }
+
+    fun outputCustomSchema() {
+        println("Printing Schema")
+        val objClass = Source::class
+        val constructor = objClass.constructors.filter { c ->
+            !c.parameters.any {
+                    p -> p.name != null && p.name!!.contains("serializationConstructorMarker")
+            }
+        }.first()
+        for (parameter in constructor.parameters) {
+                output(parameter, 0)
+        }
+    }
+
+    private fun output(parameter: KParameter, depth: Int) {
+        val objClass = parameter.type.classifier as KClass<*>
+        val optional = parameter.type.isMarkedNullable;
+        val constructor = objClass.constructors.filter { c ->
+            !c.parameters.any {
+                    p -> p.name != null && p.name!!.contains("serializationConstructorMarker")
+            }
+        }.first()
+        println(" ".repeat(depth) + "${parameter.name} $objClass $optional")
+        for (parameter in constructor.parameters) {
+            output(parameter, depth + 2)
+        }
     }
 }
--- parseview/src/main/java/parseview/Launcher.java
+++ parseview/src/main/java/parseview/Launcher.java
@@ -8,6 +8,7 @@ public class Launcher {
     // This class is needed due to javafx dying when launching a Application as main.
     // So this class should be used as entry point
     public static void main(String[] args) {
+        Schema.INSTANCE.outputCustomSchema();
         Schema.INSTANCE.outputSchema(new File("schema.json"));
     }
 }
