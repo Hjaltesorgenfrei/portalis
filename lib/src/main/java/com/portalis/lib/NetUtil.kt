75bb10018ba4c28ca35fc3cc00f9632eb6c4c9f2
Hjalte Sorgenfrei Mac Dalland <hjaltesorgenfrei@gmail.com>
2022-04-25 20:23:47 +0200

    Add cookie handling

--- lib/src/main/java/com/portalis/lib/NetUtil.kt
+++ lib/src/main/java/com/portalis/lib/NetUtil.kt
@@ -1,13 +1,13 @@
 package com.portalis.lib
 
-import okhttp3.Callback
-import okhttp3.OkHttpClient
-import okhttp3.Request
-import okhttp3.Response
+import okhttp3.*
 import java.io.IOException
 
 class NetUtil {
     companion object {
+        private val cookieJar = Cooker()
+        private var client = OkHttpClient.Builder().cookieJar(cookieJar).build()
+
         @Throws(IOException::class)
         fun run(url: String, callback: Callback) {
             val request: Request = Request.Builder()
@@ -22,6 +22,27 @@ class NetUtil {
                 .url(url)
                 .build()
             return OkHttpClient().newCall(request).execute().body?.string()
+        }
+    }
+}
+
+class Cooker : CookieJar {
+    var savedCookies: MutableMap<String, List<Cookie>> = mutableMapOf()
+
+    override fun loadForRequest(url: HttpUrl): List<Cookie> {
+        synchronized(savedCookies) {
+            return savedCookies.getOrDefault(url.host, emptyList())
+        }
+    }
+
+    override fun saveFromResponse(url: HttpUrl, cookies: List<Cookie>) {
+        synchronized(savedCookies) {
+            val oldCookies = savedCookies.getOrDefault(url.host, emptyList())
+            savedCookies[url.host] = oldCookies.filter { oldCookie ->
+                cookies.all { cookie ->
+                    oldCookie.name != cookie.name
+                }
+            }.plus(cookies)
         }
     }
 }
